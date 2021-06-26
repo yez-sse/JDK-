24cd8a9a564b07d52ec7657d408bbb9e26aed013
yez-sse <1429658313@qq.com>
2021-06-26 17:52:57 +0800

    lc35
    Signed-off-by: yez-sse <1429658313@qq.com>

---
+++ src/problem/lc/P35二分查找.java
@@ -0,0 +1,38 @@
+package problem.lc;
+
+public class P35二分查找 {
+    public int binarySearch(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    // 最经典的二分查找你都不会，你他妈傻逼啊卧槽，你还想他妈什么其他的卧槽
+    // public int searchInsert(int[] nums, int target) {
+    //     int len = nums.length;
+    //     return binarySearch(nums, 0, len - 1, target);
+    // }
+
+    // public int binarySearch(int[] nums, int left, int right, int target) {
+    //     int ans = 0;
+    //     while (left <= right) {
+    //         int mid = (left + right) / 2;
+    //         if (nums[mid] == target) {
+    //             return mid;
+    //         } else if (nums[mid] < target) {
+    //             left = mid + 1;
+    //         } else {
+    //             right = mid - 1;
+    //         }
+    //         ans = mid;
+    //     }
+    //     return ans;
+    // }
+}

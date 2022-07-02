194e636079fa13e17a0a6cb4edea321e1fb449ba
yez-sse <1429658313@qq.com>
2022-07-02 14:43:13 +0800

    0702

--- src/problem/lc/lc1/P34非递减即递增数组中一数的区间_二分查找_典_变.java
+++ src/problem/lc/lc1/P34非递减即递增数组中一数的区间_二分查找_典_变.java
@@ -1,6 +1,35 @@
 package problem.lc.lc1;
 
-public class P34二分查找 {
+public class P34非递减即递增数组中一数的区间_二分查找_典_变 {
+
+
+    //我写的，是对的
+    public int[] searchRange3(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int[] res = new int[]{-1, -1};
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                while (mid >= 0 && nums[mid] == target) {
+                    mid--;
+                }
+                res[0] = Math.max(0, mid);
+                while (mid + 1 < nums.length && nums[mid+1] == target) {
+                    mid++;
+                }
+                res[1] = mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return res;
+    }
+
+
+
     public int[] searchRange(int[] nums, int target) {
         int leftIdx = binarySearch(nums, target, true);
         int rightIdx = binarySearch(nums, target, false) - 1;

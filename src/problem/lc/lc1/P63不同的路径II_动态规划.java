28592789e22431f8916faf3103de666f497b9e15
yez-sse <1429658313@qq.com>
2022-07-04 20:30:31 +0800

    0704

---
+++ src/problem/lc/lc1/P63不同的路径II_动态规划.java
@@ -0,0 +1,33 @@
+package problem.lc.lc1;
+
+public class P63不同的路径II_动态规划 {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid[obstacleGrid.length - 1][obstacleGrid[0].length - 1] == 1) {
+            return 0;
+        }
+        int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
+        for (int i = 0; i < obstacleGrid.length; i++) {
+            if (obstacleGrid[i][0] == 1) {
+                break;
+            }
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < obstacleGrid[0].length; j++) {
+            if (obstacleGrid[0][j] == 1) {
+                break;
+            }
+            dp[0][j] = 1;
+        }
+        for (int i = 1; i < obstacleGrid.length; i++) {
+            for (int j = 1; j < obstacleGrid[0].length; j++) {
+                if (obstacleGrid[i - 1][j] == 0) {
+                    dp[i][j] = dp[i - 1][j];
+                }
+                if (obstacleGrid[i][j - 1] == 0) {
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+        return dp[obstacleGrid.length - 1][obstacleGrid[0].length - 1];
+    }
+}

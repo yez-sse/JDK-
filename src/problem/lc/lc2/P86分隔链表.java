0ef1b5013ca6dab87fdb1abc09a845859fe13601
yez-sse <1429658313@qq.com>
2022-07-06 23:32:18 +0800

    0706

---
+++ src/problem/lc/lc2/P86分隔链表.java
@@ -0,0 +1,39 @@
+package problem.lc.lc2;
+
+import problem.one.ListNode;
+
+public class P86分隔链表 {
+    public ListNode partition(ListNode head, int x) {
+        if (head == null) {
+            return null;
+        }
+        ListNode newHead = new ListNode(-1);
+        ListNode tail1 = newHead;
+        ListNode tail2 = newHead;
+        ListNode p = head;
+        ListNode q = p.next;
+        while (p != null) {
+            if (p.val < x) {
+                p.next = tail1.next;
+                tail1.next = p;
+                p = q;
+                if (q != null) {
+                    q = q.next;
+                }
+                tail1 = tail1.next;
+                if (tail2.next != null && tail2.next.val < x) {
+                    tail2 = tail1;
+                }
+            } else {
+                p.next = tail2.next;
+                tail2.next = p;
+                p = q;
+                if (q != null) {
+                    q = q.next;
+                }
+                tail2 = tail2.next;
+            }
+        }
+        return newHead.next;
+    }
+}

277136a94848b89f03d40e2798e00c50f94861c2
yez-sse <1429658313@qq.com>
2022-07-06 21:10:08 +0800

    0706

---
+++ src/problem/lc/lc2/P82删链表重复项.java
@@ -0,0 +1,25 @@
+package problem.lc.lc2;
+
+import problem.one.ListNode;
+
+public class P82删链表重复项 {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode newHead = new ListNode(-1);    //有时候要借助一个新头部
+        newHead.next = head;
+        ListNode p = newHead;
+        ListNode q = head;
+        while (q != null && q.next != null) {   //这里注意不要漏掉了q != null
+            if (q.val == q.next.val) {
+                while (q.next != null && q.val == q.next.val) {
+                    q.next = q.next.next;
+                }
+                p.next = q.next;
+                q = q.next;
+            } else {
+                q = q.next;
+                p = p.next;
+            }
+        }
+        return newHead.next;
+    }
+}

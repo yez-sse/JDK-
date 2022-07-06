ae3909a8f04a0e03766b1e3e49c897ea0eea16b1
yez-sse <1429658313@qq.com>
2022-07-06 20:15:52 +0800

    0706

---
+++ src/problem/lc/lc2/P78子集_DFS_不重复_典.java
@@ -0,0 +1,31 @@
+package problem.lc.lc2;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class P78子集_DFS_不重复_典 {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3};
+        subsets(nums);
+    }
+
+    public static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Deque<Integer> path = new LinkedList<>();
+        res.add(new ArrayList<>(path));
+        dfs(nums, 0, res, path);
+        return res;
+    }
+
+    public static void dfs(int[] nums, int begin, List<List<Integer>> res, Deque<Integer> path) {
+        for (int i = begin; i < nums.length; i++) {
+            path.add(nums[i]);
+            res.add(new ArrayList<>(path)); //顺序啊，这个顺序很重要
+            dfs(nums, i + 1, res, path);
+            path.removeLast();
+        }
+    }
+}
---
+++ src/problem/lc/lc2/P79单词搜索_DFS_回溯_典.java
@@ -0,0 +1,50 @@
+package problem.lc.lc2;
+
+public class P79单词搜索_DFS_回溯_典 {
+
+    private static final int[][] directions = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+    private boolean[][] visited;
+    private char[] charArray;
+    private char[][] board;
+
+    public boolean exist(char[][] board, String word) {
+        visited = new boolean[board.length][board[0].length];
+        charArray = word.toCharArray();
+        this.board = board;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(i, j, 0, word.length())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean dfs(int i, int j, int idx, int len) {
+//        if (idx == len) {   //如果这么写的话，[["a"]] "a"，这种情况过不了
+//            return true;
+//        }
+        if (idx == len - 1) {
+            return board[i][j] == charArray[len - 1];
+        }
+        if (board[i][j] == charArray[idx]) {
+            visited[i][j] = true;
+            for (int[] direction : directions) {
+                int newI = i + direction[0];
+                int newJ = i + direction[1];
+                if (inArea(newI, newJ) && !visited[newI][newJ]) {   //注意这里是新的
+                    if (dfs(newI, newJ, idx+1, len)) {
+                        return true;
+                    }
+                }
+            }
+            visited[i][j] = false;
+        }
+        return false;
+    }
+
+    public boolean inArea(int i, int j) {
+        return i >= 0 && i < board.length && j >= 0 && j < board[0].length;
+    }
+}

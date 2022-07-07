6c3d1c6610da3339fa3d4b23f450f338e61626d4
yez-sse <1429658313@qq.com>
2022-07-07 20:57:05 +0800

    0707

---
+++ src/problem/lc/lc2/P91_解码方法_动态规划_典.java
@@ -0,0 +1,46 @@
+package problem.lc.lc2;
+
+public class P91_解码方法_动态规划_典 {
+
+    public static void main(String[] args) {
+        String s = "2101";
+        numDecodings(s);
+    }
+
+    public static int numDecodings(String s) {
+        char[] chars = s.toCharArray();
+        if (chars[0] == '0') {
+            return 0;
+        }
+
+        int[] dp = new int[chars.length];
+        dp[0] = 1;
+        if (chars.length > 1) {
+            if (chars[1] == '0' && (chars[0] - '0') * 10 + (chars[1] - '0') > 26) {
+                dp[1] = 0;
+            } else if (chars[1] == '0' || (chars[0] - '0') * 10 + (chars[1] - '0') > 26) {
+                dp[1] = 1;
+            } else {
+                dp[1] = 2;
+            }
+        }
+        for (int i = 2; i < chars.length; i++) {
+            if (chars[i] == '0') {
+                if (chars[i - 1] == '0' || ((chars[i - 1] - '0') * 10 + (chars[i] - '0') > 26)) {
+                    return 0;
+                } else {
+                    dp[i] = dp[i - 2];
+                }
+            } else if (chars[i - 1] == '0') {
+                dp[i] = dp[i - 1];
+            } else  {
+                if ((chars[i - 1] - '0') * 10 + (chars[i] - '0') > 26) {
+                    dp[i] += dp[i - 1];
+                } else {
+                    dp[i] = dp[i - 1] + dp[i - 2];
+                }
+            }
+        }
+        return dp[chars.length - 1];
+    }
+}
